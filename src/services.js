dc5e43bccfdec7a7f211f5e4f7ea1a3bdaf1a804
Anastasia Xin <yeeching.hsin@gmail.com>
2023-03-06 17:04:56 -0500

    Invoked setTimeout

--- src/services.js
+++ src/services.js
@@ -1,5 +1,3 @@
 export function fetchWithTimeout(delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, delay);
-  });
+  return new Promise((resolve, reject) => setTimeout(resolve, delay));
 }
