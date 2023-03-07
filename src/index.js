1a138f84ae8a587307f763912f44fa23d2c69ed4
Anastasia Xin <yeeching.hsin@gmail.com>
2023-03-07 15:20:53 -0500

    Refactoring

--- src/index.js
+++ src/index.js
@@ -3,27 +3,29 @@ import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 const movies = require('./data/movies.json');
 
 function getBooksAndMovies() {
-  return Promise.all([fetchMovies(), fetchBooks()]);
+  return Promise.all([fetchMovies(), fetchBooks()])
+    .then(([movies, books]) => {
+      movies, books;
+    })
+    .catch((error) => console.log('Error fetching movies and books ', error));
 }
 
-const getBooksAndMoviesPromise = getBooksAndMovies()
-  .then(([movies, books]) => {
-    movies, books;
-  })
-  .catch((error) => console.log('Error fetching movies and books ', error));
+const getBooksAndMoviesPromise = getBooksAndMovies();
 
 getBooksAndMoviesPromise.then((results) =>
   console.log('getBooksAndMoviesPromise ', results)
 );
 
-function getMoviesOrBooks() {
-  return Promise.race([fetchMovies(), fetchBooks()]);
+function getBooksOrMovies() {
+  return Promise.race([fetchMovies(), fetchBooks()])
+    .then((results) => results)
+    .catch((error) =>
+      console.log('Error waiting for the promise race ', error)
+    );
 }
 
-const getMoviesOrBooksPromise = getMoviesOrBooks()
-  .then((results) => results)
-  .catch((error) => console.log('Error waiting for the promise race ', error));
+const getBooksOrMoviesPromise = getBooksOrMovies();
 
-getMoviesOrBooks.then((results) =>
+getBooksOrMovies.then((results) =>
   console.log('getBooksOrMoviesPromise ', results)
 );
